581313fc4766e84b10b5e94edf6258c1763434e3
joswin18 <136182002+joswin18@users.noreply.github.com>
2023-09-28 21:18:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+var typed = new Typed(".text", {
+    strings: ["Coca Cola"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
+*/
+
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle')
+    circle.style.background = color;
+}
